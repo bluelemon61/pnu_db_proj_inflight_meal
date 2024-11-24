80e76fc705a1ba1c68571bb78caa5ed972b3cc40
bluelemon61 <67902252+bluelemon61@users.noreply.github.com>
2024-11-24 20:49:53 +0900

    feat: crew API templete

---
+++ app/api/crew/eat-check/route.js
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+
+/**
+ * 승객들의 식사 상태를 확인한다.
+ * 
+ * 권한: 승무원(crew)
+ * 
+ * @param {*} request 
+ * @returns 
+ */
+export async function GET(request){
+  // flight_number<number>: 항공기 id
+  const {flight_number} = await request.json();
+
+  const data = null;
+
+  return new NextResponse(data, {
+    status: 200,
+  });
+}
---
+++ app/api/crew/menu/route.js
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+
+/**
+ * 기내식을 제공한다. 승무원 본인이 기내식을 먹는 경우도 이용된다.
+ * 
+ * 권한: 승무원(crew)
+ * 
+ * @param {*} request 
+ * @returns 
+ */
+export async function POST(request){
+  // flight_number<number>: 항공기 id
+  // food_id<id>: 음식 id (flight_food table)
+  // user_id<string>: 승객(승무원)의 id
+  const {flight_number, food_id, user_id} = await request.json();
+
+  const data = null;
+
+  return new NextResponse(data, {
+    status: 200,
+  });
+}
+
+/**
+ * 기내식 목록을 불러온다. (승무원용)
+ * 
+ * 권한: 승무원(crew)
+ * 
+ * @param {*} request 
+ * @returns 
+ */
+export async function GET(request){
+  // flight_number<number>: 항공기 id
+  // food_target<string>: 제공할 대상 (승객, 직원, null)
+  const {flight_number, food_target} = await request.json();
+
+  const data = null;
+
+  return new NextResponse(data, {
+    status: 200,
+  });
+}
