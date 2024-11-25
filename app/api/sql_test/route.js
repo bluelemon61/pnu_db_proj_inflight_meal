44ecb4ffb9ee23474a4e4700d7a8932aa767874a
DongHoon Lee <67902252+bluelemon61@users.noreply.github.com>
2024-11-25 11:18:07 +0900

    feat: sql_test 사용 예제 업로드

--- app/api/sql_test/route.js
+++ app/api/sql_test/route.js
@@ -1,9 +1,27 @@
 import { NextResponse } from "next/server";
+import { Client } from "pg";
 
 export async function GET(reqeust){
-
-  console.log('hi');
+  const DB_HOST = process.env.DB_HOST;
+  const DB_PORT = process.env.DB_PORT;
+  const DB_USER = process.env.DB_USER;
+  const DB_PASSWORD = process.env.DB_PASSWORD;
+  const DB_NAME = process.env.DB_NAME;
   
+  const client = new Client({
+    user: DB_USER,
+    host: DB_HOST,
+    database: DB_NAME,
+    password: DB_PASSWORD,
+    DB_PORT: DB_PORT,
+  });
+
+  client.connect();
+
+  const data = await client.query("SELECT * FROM users;");
+
+  console.log(data.rows);
+
   return new NextResponse(null, {
       status: 200,
   });
