9c125a2ec7c0fe7b7bab628f967193f1605faeab
bluelemon61 <67902252+bluelemon61@users.noreply.github.com>
2024-12-06 00:15:10 +0900

    feat: 비행기 착륙 시 승객 데이터 초기화

--- app/api/captain/airplain/route.js
+++ app/api/captain/airplain/route.js
@@ -1,24 +1,25 @@
 import { NextResponse } from "next/server";
-import { Client } from "pg";
+import { Pool } from "pg";
 
+/**
+ * 운항중인 비행기의 운항 상태를 업데이트한다.
+ * 
+ * 권한: 기장(captain)
+ * 
+ * @returns 
+ */
 export async function POST(request) {
-  const DB_HOST = process.env.DB_HOST;
-  const DB_PORT = process.env.DB_PORT;
-  const DB_USER = process.env.DB_USER;
-  const DB_PASSWORD = process.env.DB_PASSWORD;
-  const DB_NAME = process.env.DB_NAME;
-
-  const client = new Client({
-    user: DB_USER,
-    host: DB_HOST,
-    database: DB_NAME,
-    password: DB_PASSWORD,
-    port: DB_PORT,
+  const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
   });
-
+  const client = await pool.connect(); // 연결 풀에서 클라이언트 가져오기
   try {
-    await client.connect();
-
+    // * @param {number} flight_number 항공기 id
+    // * @param {string} flight_state 항공기 상태 (이륙, 순항, 난기류, ...)
     const { flight_number, flight_state } = await request.json();
 
     if (!flight_number || !flight_state) {
@@ -28,29 +29,40 @@ export async function POST(request) {
       );
     }
 
+    await client.query('BEGIN');
     const result = await client.query(
-      'UPDATE flight SET flight_state = $1 WHERE flight_number = $2 RETURNING *',
+      'UPDATE flight SET flight_state = $1 WHERE flight_number = $2 RETURNING *;',
       [flight_state, flight_number]
     );
 
-    if (result.rows.length > 0) {
-      return new NextResponse(
-        JSON.stringify({ success: true, data: result.rows[0] }),
-        { status: 200 }
-      );
-    } else {
-      return new NextResponse(
-        JSON.stringify({ success: false, message: "Flight not found" }),
-        { status: 404 }
-      );
+    if (flight_state === '착륙') {
+      const initQuery = `
+        UPDATE flight_user
+        SET
+          sleep_state = 'NORMAL',
+          eat_count = 0,
+          food_order = NULL
+        WHERE flight_number = $1
+        RETURNING *;
+      `
+      const initResult = await client.query(initQuery, [flight_number]);
+      console.log('승객 데이터 초기화 실행');
     }
+
+    client.query('COMMIT');
+
+    return new NextResponse(
+      JSON.stringify({ success: true, data: result.rows[0] || null }),
+      { status: 200 }
+    );
   } catch (error) {
     console.error("Error updating flight state:", error);
+    await client.query('ROLLBACK');
     return new NextResponse(
       JSON.stringify({ success: false, message: "Internal server error" }),
       { status: 500 }
     );
   } finally {
-    await client.end();
+    client.release();
   }
 }
